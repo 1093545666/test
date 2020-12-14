35b7f8a7889324ff2099f238b6abe27efd852eba
lenovo <qwe>
2020-12-14 16:34:35 +0800

    09a

---
+++ SpringEasy/src/main/java/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+    private String name;
+    private Double money;
+
+    public Book( ) {
+        System.out.println("Book Build");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+}
